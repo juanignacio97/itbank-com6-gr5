1582a8df4624c976de6aa8c4d68f1af2dfad50db
Nicolás Korzusehec <99510894+NicolasKorzusehec@users.noreply.github.com>
2022-06-21 00:36:32 -0300

    itbank-version-0.1 (#32)
    * Login inicial
    
    * style.css
    
    * Update README.md
    
    * header
    
    * Update README.md
    
    * Login finalizado
    
    * Incorporación de botonera de accesos directos
    
    * Incorporación apartado saldo cuenta
    
    * correccion margen
    
    * Organización del html, espaciados, renombramiento de secciones con sus respectivos id
    
    * marginado de la seccion id
    
    * Update README.md
    
    * barra busqueda
    
    * lorem
    
    * Secciones e id actualizadas, organizado archivos, posición marcada de la barra de navegacion,  aumento de resolución del logo del itba
    
    * .
    
    * Update index.html
    
    * solucción problema que creé, no me cargaban ni fotos ni styles en la webpage
    
    * .
    
    * ,
    
    * Update login.html
    
    Acomodé algunas etiquetas centrales del html
    
    * ultimo error mio corregido
    
    * Add logo pestaña Webpages
    
    * Prueba solucion background image
    
    * .
    
    * .
    
    * .
    
    * Configuracion lang prop español
    
    * - añade json con los datos de cotizacion del dolar y .gitignore (#18)
    
    * Agrega las cards (#19)
    
    * Update README.md
    
    * js-homebanking
    
    * Vinculando el fetch y funciones hechas por sofia, ordenando un poco el html
    
    * Sumo responsive design a las cards de "Cotizacion del dolar HOY"
    
    * Unifique la estetica de las cards propuesta por juan con la funcionalidad dinamica que armo sofia del fetch.  Terminan de ser dinamicas las cards con respecto a la api
    
    * Limpiando un poco el js
    
    * limpiando archivos
    
    * Hay un problema con el overflow del contenido de las cards, se nota en pantallas chicas, aparece tras vincular la data de la api por textos largos no previstos
    
    * Planteo boton de descarga de pdf y de excel, con su class que posteriormente sea el selector para definir el evento
    
    * Incorporación de función descargar PDF
    
    * Corrección de error de conversión
    
    * NN
    
    * Solucion problemas responsive design de la seccion cotizacion dolar
    
    * tabeando un poco los archivos. Por alguna razon la funcion descargarPdf actua correctamente desde el live server pero no del page
    
    * .
    
    * Estructurando un poco el README
    
    * Configure una vista default para que si no carga la seccion no quede vacio. A futuro Podemos trabajar las modificaciones directamente desde nodos especificos para que no recorra todo el section y lo re imprima. Podriamos eliminar la funcion actualizatr y directamente vincular el evento de click en actualizar a la funcion cotizacionDolar para que solo la vuelva a ejecutar
    
    * Sofia (#24)
    
    Co-authored-by: SofiaEna <sofia.maglioto@hotmail.com>
    Co-authored-by: juanignacio97 <juanignacioledesma97@gmail.com>
    
    * Update README.md
    
    * Retoque a barra-busqueda, saldo de cuenta, agrega ult. movimientos (#33)
    
    Co-authored-by: “igabrielli” <igabrielli@hotmail.com>
    Co-authored-by: juanignacio97 <juanignacioledesma97@gmail.com>
    Co-authored-by: Juan Ignacio Ledesma <66830554+juanignacio97@users.noreply.github.com>
    Co-authored-by: Elias Upstein <eliasupstein@gmail.com>
    Co-authored-by: nicolas <nicomoly525@gmail.com>
    Co-authored-by: SofiaEna <sofia.maglioto@hotmail.com>
    Co-authored-by: SofiaEna <99747832+SofiaEna@users.noreply.github.com>

---
+++ js/main.js
@@ -0,0 +1,87 @@
+const api_url = 'https://www.dolarsi.com/api/api.php?type=valoresprincipales';   //API para las cotizaciones
+
+let sectionCotizaciones = document.querySelector('#cotizacion-dolar .container .row');
+
+//Función que agrega dinámicamente a la página contenido de cotizaciones
+async function cotizacionDolar(){
+   const fetching = await fetch(api_url);
+   const data = await fetching.json();
+
+   //Extrae el último elemento del array que contiene la fecha y hora de actualización
+   let fecha = data.pop();
+   //Extrae el último elemento del array que contiene una cotización sin nombre
+   let dolar = data.pop();
+   //Extrae la cotización del bitcoin que no aplica
+   let bitcoin =data.splice(5, 1);
+   console.log(data);
+
+   //Pendiente agregar en formato
+   //  <p>Variacion: ${caso.casa.variacion}</p>
+   sectionCotizaciones.innerHTML = "";
+   for (let caso of data) {
+      let correccionDecimal =caso.casa.compra;
+      console.log(typeof correccionDecimal !== "string");
+      //tratando corregir que solo hayan 2 decimales, no hubo cambios
+      if (typeof correccionDecimal !== "string") {
+         correccionDecimal = Number(caso.casa.compra).toFixed(2);
+      };
+   sectionCotizaciones.innerHTML+= `
+   <div class="col-12 col-sm-6 col-md-4">
+      <div class="card text-center m-2">
+         <div class="card-header">
+            <i class="fa-solid fa-money-bill-wave" style="color:green"></i> 
+            ${caso.casa.nombre.toUpperCase()}
+         </div>
+         <div class="row card-body">
+            <div class="col-6 col-sm-12 col-xl-6">
+                  <p class="card-text">COMPRA</p>
+                  <h3 class="card-title">${correccionDecimal}</h3>
+            </div>
+            <div class="col-6 col-sm-12 col-xl-6">
+                  <p class="card-text">VENTA</p>
+                  <h3 class="card-title">${caso.casa.venta}</h3>
+            </div>
+         </div>
+         <div class="card-footer text-muted">
+         ACTUALIZADO: ${fecha.casa.fecha} ${fecha.casa.recorrido}
+         </div>
+      </div>
+   </div>`
+}};
+
+//Función que agrega la funcionalidad de descargar el pdf del ID seleccionado. Utiliza la librería HTML2PDF
+//HTML2PDF: junta las 2 librerías html2canvas y jsPDF
+function descargarPdf() {
+   const descargar = document.getElementById("cotizacion-dolar");
+
+   html2pdf()
+      .set({
+         margin: 1,
+         filename: "Cotizaciones.pdf",
+         image: {
+            type: "jpeg",
+            quality: 0.90,
+         },
+         //Configuraciones del estilo del pdf (para mantener el original del HTML). Librería html2canvas
+         html2canvas: {     
+            scale: 7,
+            letterRendering: true,
+         },
+         //Configuraciones del propio pdf. Librería jsPDF
+         jsPDF: {           
+            unit: "cm",
+            format: "a4",
+            orientation: "portrait",
+         }
+      })
+      .from(descargar)
+      .save()
+      .finally();
+};
+
+function actualizar(){
+   sectionCotizaciones.innerHTML="";
+   cotizacionDolar();
+};
+
+document.addEventListener("DOMContentLoaded", cotizacionDolar, false);
